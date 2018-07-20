6a2cbaefe6ddea5ec3be34826ebabacd455ac004
Zach <zfaust88@gmail.com>
2018-07-20 11:47:13 -0500

    Add project files.

---
+++ HotterWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HotterWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
